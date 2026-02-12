a187ebc3adb994ee5336be35d76b195c79e130ed
ahetru <axel.a.hetru@gmail.com>
2026-02-12 14:37:55 +0100

    fix(BitcoinExchange): move default constructor; copy constructor; assignement operator to private

--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -7,16 +7,18 @@ class BitcoinExchange
 {
 	private:
 		std::map<std::string, double> _referenceDB;
+
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& other);
+		BitcoinExchange& operator=(const BitcoinExchange& other);
+
 		void loadDatabase(const std::string &filename);
 		bool isValidValue(const std::string &value, double &out) const;
 		bool isValidDate(const std::string &date) const;
 
 	public:
 		BitcoinExchange(const std::string &dbFilename);
-		BitcoinExchange();
 		~BitcoinExchange();
-		BitcoinExchange(const BitcoinExchange& other);
-		BitcoinExchange& operator=(const BitcoinExchange& other);
 
 		void processInput(const std::string& filename) const;
 };
